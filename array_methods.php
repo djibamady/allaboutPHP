ee255439c8db5d5c4d9ad9c0341a9f716fbd933e
djibamady <mohamedfofana2k19@gmail.com>
2023-06-05 19:42:45 +0200

    array_search  get the position of saving

--- array_methods.php
+++ array_methods.php
@@ -12,5 +12,7 @@ if (in_array('Mohamed FOFANA', $users)) {
 if (in_array('Arlette Chabot', $users)) {
     echo 'Arlette fait bien partie des utilisateurs enregistrés !';
 } else {
-    echo 'Arlette ne fait pas partie des utilisateurs enregistrés !';
+    echo 'Arlette ne fait pas partie des utilisateurs enregistrés !<br>';
 }
+$positionFOFANA = array_search('Mohamed FOFANA', $users);
+echo '"Mohamed FOFANA" se trouve en position ' . $positionFOFANA . PHP_EOL;
