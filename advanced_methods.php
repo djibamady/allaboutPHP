e383176f53fadfee4a9679ae68343c75678a45e6
djibamady <mohamedfofana2k19@gmail.com>
2023-06-05 20:19:27 +0200

    uses the str_replace

--- advanced_methods.php
+++ advanced_methods.php
@@ -12,7 +12,15 @@ function isAllowed($array)
 }
 $isAllow = isAllowed($recipe);
 if ($isAllow) {
-    echo 'La recette doit être affichée !';
+    echo 'La recette doit être affichée !<br>';
 } else {
-    echo 'La recette doit être cachée !';
+    echo 'La recette doit être cachée !<br>';
 }
+// Texte d'origine
+$texte = "Le chat est sur le tapis.";
+
+// Recherche et remplacement
+$nouveauTexte = str_replace("chat", "chien", $texte);
+
+// Affichage du résultat
+echo $nouveauTexte;
