227d56607c920de4170e11707c31bcbc9bb2e094
djibamady <mohamedfofana2k19@gmail.com>
2023-06-05 09:56:29 +0200

    all about condition

---
+++ condition.php
@@ -0,0 +1,12 @@
+<p>
+
+    Les conditions permettent à PHP de prendre des décisions en fonction de la valeur des variables.
+
+    La forme de condition la plus courante est if ... elseif ... else qui signifie « si »… « sinon si »… « sinon ».
+
+    On peut combiner des conditions avec les instructions&& (« et ») et|| (« ou »).
+
+    Si une condition comporte de nombreux elseif , il peut être plus pratique d'utiliser la condition switch.
+
+    Les ternaires sont des conditions condensées qui font un test sur une variable, et en fonction des résultats de ce test, elles donnent une valeur à une autre variable. Elles sont cependant plus rarement utilisée
+</p>
