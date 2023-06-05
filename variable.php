ec1722484d63abfd598ed1304007e23fc88121b6
djibamady <mohamedfofana2k19@gmail.com>
2023-06-05 09:35:44 +0200

    All about $variable

---
+++ variable.php
@@ -0,0 +1,11 @@
+<p>
+    Une variable est une petite information qui reste stockée en mémoire le temps de la génération de la page PHP. Elle a un nom et une valeur.
+
+    Il existe plusieurs types de variables qui permettent de stocker différents types d'informations : du texte ( string ), des nombres entiers ( int ), des nombres décimaux (float), des booléens pour stocker vrai ou faux ( bool ), etc.
+
+    En PHP, un nom de variable commence par le symbole dollar : $age , par exemple.
+
+    La valeur d'une variable peut être affichée avec l'instruction echo .
+
+    Il est possible de faire des calculs mathématiques entre plusieurs variables : addition, soustraction, multiplication…
+</p>
