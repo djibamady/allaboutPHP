54569966a5a809e8ad492a7e9b0f7779c59d291a
djibamady <mohamedfofana2k19@gmail.com>
2023-06-05 08:36:49 +0200

    ECHO WITH BOLD AND DOUBLE CLAW

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php echo "Ceci est du <strong>texte</strong>";
-echo "Cette ligne a été écrite \"uniquement\" en PHP.";
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Notre première instruction : echo</title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <h2>Affichage de texte avec PHP</h2>
+
+    <p>
+        Cette ligne a été écrite entièrement en HTML.<br />
+        <?php echo ("Celle-ci a été écrite entièrement en PHP.");
+        echo "Ceci est du <strong>texte</strong>";
+        echo "Cette ligne a été écrite \"uniquement\" en PHP.";
+        ?>
+    </p>
+</body>
+
+</html>
