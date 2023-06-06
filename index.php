4b30df6f58d5785f382e78561a5d1f6bdafed92f
djibamady <mohamedfofana2k19@gmail.com>
2023-06-06 13:37:37 +0200

    send message with form index,submit_contact

--- index.php
+++ index.php
@@ -31,6 +31,14 @@
     </div>
 
     <?php include_once('footer.php'); ?>
+    <?php
+    if (
+        (!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL))
+        || (!isset($_GET['message']) || empty($_GET['message']))
+    ) {
+        echo ('Il faut un email et un message valides pour soumettre le formulaire.');
+    }
+    ?>
 </body>
 
 </html>
