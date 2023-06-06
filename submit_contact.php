2d69715722bc05f43324600e1804cd38bb13c02b
djibamady <mohamedfofana2k19@gmail.com>
2023-06-06 13:40:04 +0200

    htmlspecialchars to protect data against injection

--- submit_contact.php
+++ submit_contact.php
@@ -14,8 +14,8 @@ if (
 
         <div class="card-body">
             <h5 class="card-title">Rappel de vos informations</h5>
-            <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
-            <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
+            <p class="card-text"><b>Email</b> : <?php echo htmlspecialchars($_GET['email']); ?></p>
+            <p class="card-text"><b>Message</b> : <?php echo htmlspecialchars($_GET['message']); ?></p>
         </div>
     </div>
 <?php
