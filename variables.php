2bfa322f4f9adeb23294b017771569e18c76855b
djibamady <mohamedfofana2k19@gmail.com>
2023-06-06 23:45:25 +0200

    users data

---
+++ variables.php
@@ -0,0 +1,62 @@
+$users = array(
+array(
+'full_name' => 'Mohamed FOFANA',
+'email' => 'mohamedfofana35700@gmail.com',
+'age' => 28,
+'password' => '123456'
+),
+array(
+'full_name' => 'John Doe',
+'email' => 'johndoe@example.com',
+'age' => 35,
+'password' => 'abcdef'
+),
+array(
+'full_name' => 'Jane Smith',
+'email' => 'janesmith@example.com',
+'age' => 42,
+'password' => 'qwerty'
+),
+array(
+'full_name' => 'Alice Johnson',
+'email' => 'alicejohnson@example.com',
+'age' => 30,
+'password' => 'password123'
+),
+array(
+'full_name' => 'Bob Williams',
+'email' => 'bobwilliams@example.com',
+'age' => 50,
+'password' => 'pass123'
+),
+array(
+'full_name' => 'Sarah Davis',
+'email' => 'sarahdavis@example.com',
+'age' => 27,
+'password' => 'test456'
+),
+array(
+'full_name' => 'Michael Miller',
+'email' => 'michaelmiller@example.com',
+'age' => 38,
+'password' => 'mypass'
+),
+array(
+'full_name' => 'Emily Brown',
+'email' => 'emilybrown@example.com',
+'age' => 33,
+'password' => 'pass1234'
+),
+array(
+'full_name' => 'David Wilson',
+'email' => 'davidwilson@example.com',
+'age' => 45,
+'password' => 'davidpass'
+),
+array(
+'full_name' => 'Olivia Taylor',
+'email' => 'oliviataylor@example.com',
+'age' => 29,
+'password' => 'mypassword'
+)
+);
