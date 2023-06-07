f2aabd8431f078117e66ec3ad1057c5ad601b74c
djibamady <mohamedfofana2k19@gmail.com>
2023-06-07 02:44:59 +0200

    show receipt author wi database

---
+++ showreceiptauthor.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Affichage des recettes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Affichage des recettes</h1>
+        <!-- Boucle sur les recettes -->
+        <?php
+        $mysqlClient = include_once('config/connexion.php');
+
+
+
+        // On récupère tout le contenu de la table recipes
+        $sqlQuery = 'SELECT * FROM recipes';
+        $recipesStatement = $mysqlClient->prepare($sqlQuery);
+        $recipesStatement->execute();
+        $recipes = $recipesStatement->fetchAll();
+
+        // On affiche chaque recette une à une
+        foreach ($recipes as $recipe) {
+        ?>
+            <p><?php echo $recipe['title'] . " est crée par :"; ?>
+                <?php echo $recipe['author']; ?></p>
+        <?php
+        }
+        ?>
+
+        <?php require_once 'variables.php'; ?>
+
+    </div>
+</body>
+
+</html>
